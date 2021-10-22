a61fc2742fbc7f69c6bff55f166ba401a02a16e1
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2021-10-22 14:28:53 -0400

    Added original Frames to loadAndCount benchmark

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -15,26 +15,36 @@ import qualified Frames.Streamly.Streaming.Interface as Stream
 import qualified Frames.Streamly.Streaming.Pipes as StreamP
 import qualified Frames.Streamly.Streaming.Streamly as StreamS
 
-import qualified Frames
+import qualified Frames hiding (inCoreAoS)
 import Frames.CSV as Frames
 import Frames.TH as Frames
 import Frames.InCore as Frames
 
 import qualified Control.Foldl as FL
 
-FStreamly.tableTypes' ffRowGen
+FStreamly.tableTypes' ffNewRowGen
+Frames.tableTypes' ffRowGen
 
 loadAndCount :: Stream.StreamFunctionsWithIO s IO -> IO Int
 loadAndCount sfWIO = do
   let sf = Stream.streamFunctions sfWIO
       fLength = FL.fold FL.length
   forestFiresPath <- Paths.usePath Paths.forestFiresPath
-  forestFires :: Frames.Frame FF <- FStreamly.inCoreAoS sf $ FStreamly.readTableOpt sfWIO fFParser forestFiresPath
+  forestFires :: Frames.Frame FFNew <- FStreamly.inCoreAoS sf $ FStreamly.readTableOpt sfWIO fFNewParser forestFiresPath
   return $ fLength forestFires
+
+loadAndCountF :: IO Int
+loadAndCountF = do
+  let fLength = FL.fold FL.length
+  forestFiresPath <- Paths.usePath Paths.forestFiresPath
+  forestFires :: Frames.Frame FF <- Frames.runSafeT $ Frames.inCoreAoS $ Frames.readTableOpt fFParser forestFiresPath
+  return $ fLength forestFires
+
 
 main = defaultMain [
   bgroup "loadAndCount" [ bench "Pipes" $ nfIO (loadAndCount StreamP.pipesFunctionsWithIO)
                         , bench "Streamly" $ nfIO (loadAndCount StreamS.streamlyFunctionsWithIO)
+                        , bench "Frames" $ nfIO loadAndCountF
                         ]
 
   ]
--- bench/Paths.hs
+++ bench/Paths.hs
@@ -4,11 +4,12 @@ module Paths where
 
 import qualified Paths_Frames_streamly as Paths
 import qualified Frames.Streamly.TH as FStreamly
-import qualified Frames.ColumnUniverse as Frames
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import qualified  Data.Text as T
 import qualified Frames.Streamly.TH as FStreamly
+import qualified Frames
+import qualified Frames.TH as Frames
 
 forestFiresPath :: FilePath
 forestFiresPath = "forestFires.csv"
@@ -19,15 +20,18 @@ forestFiresNoHeaderPath = "forestFiresNoHeader.csv"
 forestFiresFewerColsPath :: FilePath
 forestFiresFewerColsPath = "forestFiresFewerCols.csv"
 
-
 thPath :: FilePath -> FilePath
 thPath x = "./example_data/" ++ x
 
 usePath :: FilePath -> IO FilePath
 usePath x =  fmap (\dd -> dd ++ "/" ++ x) Paths.getDataDir
 
-ffRowGen :: FStreamly.RowGen FStreamly.DefaultStream 'FStreamly.ColumnByName Frames.CommonColumns
-ffRowGen = (FStreamly.rowGen (thPath forestFiresPath)) { FStreamly.rowTypeName = "FF" }
+
+ffRowGen :: Frames.RowGen Frames.CommonColumns
+ffRowGen = (Frames.rowGen (thPath forestFiresPath)) { Frames.rowTypeName = "FF" }
+
+ffNewRowGen :: FStreamly.RowGen FStreamly.DefaultStream 'FStreamly.ColumnByName Frames.CommonColumns
+ffNewRowGen = (FStreamly.rowGen (thPath forestFiresPath)) { FStreamly.rowTypeName = "FFNew" }
 
 ffColSubsetRowGen :: FStreamly.RowGen FStreamly.DefaultStream 'FStreamly.ColumnByName Frames.CommonColumns
 ffColSubsetRowGen = FStreamly.modifyColumnSelector modSelector rowGen
